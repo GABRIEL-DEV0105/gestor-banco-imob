928d17f8f1a7d9bf0add132d1babf567b44830b8
Gabriel de Oliveira <gabriel.dev0105@gmail.com>
2025-03-23 12:35:37 -0300

    feat: adicionando fluxo de rotas e suas dependencias

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/pages/Home';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ButtonTransaction from './src/components/ButtonTransaction';
 
 export default function App() {
+  const Stack = createStackNavigator();
+  
   return (
-      <Home/>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="NewTransaction" component={ButtonTransaction} options={{ title: 'Nova Transação' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
